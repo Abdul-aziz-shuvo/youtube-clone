a527f287e0b37338be836105ca3230dcb23e7ad4
Abdul-aziz-shuvo <shuvoahmedkhan202@gmail.com>
2021-05-30 18:10:05 +0600

    firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyCnjnZuzbpblEUGFtA7KBLW_5nZCHypD8g",
+    authDomain: "shuvo-yt-clone.firebaseapp.com",
+    projectId: "shuvo-yt-clone",
+    storageBucket: "shuvo-yt-clone.appspot.com",
+    messagingSenderId: "702644085700",
+    appId: "1:702644085700:web:bb9f1699a4b1d1da233a91"
+};
+firebase.initializeApp(firebaseConfig)
