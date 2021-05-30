37fefac2f49b8acb7dd81016ecc770f37560c57f
Abdul-aziz-shuvo <shuvoahmedkhan202@gmail.com>
2021-05-30 18:10:18 +0600

    App component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,54 @@ import { Container } from 'react-bootstrap'
 import Header from './components/header/Header'
 import Homescreen from './components/screens/homescreen/Homescreen';
 import Sidebar from './components/sidebar/Sidebar'
+import './_app.scss'
+import {useState} from "react";
+import LoginScreen from "./components/screens/loginscreen/LoginScreen";
+import {BrowserRouter as Router,Route,Switch,Redirect} from 'react-router-dom'
 
-
-function App() {
+const Layout = ({children})  => {
+    const [sidebar,setSidebar] = useState(false)
+    const toggleSidebar = () => {
+        setSidebar((prevState) => (
+            !prevState
+        ))
+    }
   return (
     <div className="App">
-        <Header/>
+        <Header toggleSidebar={toggleSidebar}/>
         <div className="app_container">
-            <Sidebar />
+            <Sidebar sidebar={sidebar} toggleSidebar={toggleSidebar}/>
             <Container fuild className="app_main">
-              <Homescreen/>
+                {children}
             </Container>
         </div>
+
     </div>
   );
 }
 
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path='/' exact>
+                    <Layout>
+                        <Homescreen/>
+                    </Layout>
+                </Route>
+                <Route path='/auth' >
+                    <LoginScreen/>
+                </Route>
+                <Route path='/search'>
+                    <Layout>
+                        <h3 style={{color:'white'}}>Search</h3>
+                    </Layout>
+                </Route>
+                <Route>
+                    <Redirect to='/'/>
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
 export default App;
