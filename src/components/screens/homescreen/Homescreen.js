00de79b2a97c55cf072d9c68a232f1591b1718b4
Abdul-aziz-shuvo <shuvoahmedkhan202@gmail.com>
2021-05-30 18:08:48 +0600

    HomeScreen component and style

--- src/components/screens/homescreen/Homescreen.js
+++ src/components/screens/homescreen/Homescreen.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import {Col, Container, Row} from 'react-bootstrap'
 import CategoriesBar from '../../categoriesBar/CategoriesBar'
+import Video from "../../video/Video";
 
 const Homescreen = () => {
     return (
         <div>
             <Container>
                 <CategoriesBar/>
+                <Row>
+                    {
+                        [...new Array(20)].map(() => (
+                            <Col lg={3} md={4}>
+                                <Video/>
+                            </Col>
+                        ))
+                    }
+                </Row>
             </Container>
         </div>
     )
