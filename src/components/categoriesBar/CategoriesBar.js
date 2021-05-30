a7a0f806ea2d662df27724bd08f66fe3c1268726
Abdul-aziz-shuvo <shuvoahmedkhan202@gmail.com>
2021-05-30 18:06:51 +0600

    category component and style

--- src/components/categoriesBar/CategoriesBar.js
+++ src/components/categoriesBar/CategoriesBar.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./_categories.scss"
 const  CategoriesBar = () => {
+
+    const keywords = [
+        'All',
+        'React js',
+        'Angular js',
+        'React Native',
+        'use of API',
+        'Redux',
+        'Music',
+        'Algorithm Art ',
+        'Guitar',
+        'Bengali Songs',
+        'Coding',
+        'Cricket',
+        'Football',
+        'Real Madrid',
+        'Gatsby',
+        'Poor Coder',
+        'Shwetabh',
+    ]
+
+    const [active,setActive] = useState('All');
+
+    const handleClick = (value) => {
+
+        setActive(value)
+    }
+
+
+
+
     return (
-        <div>
-            <h3>CategoriesBar</h3>
+        <div className='categoriesBar'>
+            {keywords.map((value, i) => (
+                <span key={i} onClick={() => handleClick(value)}
+                   className={active === value ? 'active' : ''}>{value}</span>
+            ))}
         </div>
     )
 }
