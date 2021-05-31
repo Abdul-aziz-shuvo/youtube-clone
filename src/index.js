a578c91c3c853f26800c7c2f2c79f1e0e8f3e458
Abdul-aziz-shuvo <shuvoahmedkhan202@gmail.com>
2021-05-31 19:00:41 +0600

    bring Router file - take from App file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
 import './_base.scss'
+import store from './redux-store/store'
+import {Provider} from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <Provider store={store}>
+      <Router>
+          <App />
+      </Router>
+   </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
