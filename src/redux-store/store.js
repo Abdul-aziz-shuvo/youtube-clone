7ffd50e3e5079d87a750291ada0d5513684218df
Abdul-aziz-shuvo <shuvoahmedkhan202@gmail.com>
2021-05-31 18:58:18 +0600

    redux store

---
+++ src/redux-store/store.js
@@ -0,0 +1,16 @@
+import {createStore,applyMiddleware,combineReducers} from 'redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {authReducer} from '../redux-store/reducers/auth.reducer'
+
+const rootReducer = combineReducers({
+    auth : authReducer
+})
+
+const  store = createStore(rootReducer,{},composeWithDevTools(applyMiddleware(thunk)))
+
+export default store
+
+
+
+
