c45e59d4e8b7257e5e6de4d6a6ae314061a4f17f
Abdul-aziz-shuvo <shuvoahmedkhan202@gmail.com>
2021-05-31 18:59:21 +0600

    login functionality

--- src/components/screens/loginscreen/LoginScreen.js
+++ src/components/screens/loginscreen/LoginScreen.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import {useDispatch,useSelector} from 'react-redux'
+import {useHistory} from 'react-router-dom'
 import './_LoginScreen.scss'
+import {login} from '../../../redux-store/actions/auth.action'
+
+
 const LoginScreen = () => {
+    const dispatch = useDispatch()
+    const history = useHistory();
+    const accesstoken = useSelector((state) => {
+        return state.auth.accessToken;
+    })
+    const handleLogin = () =>{
+       dispatch(login())
+
+    }
+    useEffect(() => {
+        if (accesstoken) {
+           return  history.push('/')
+        }
+    },[history,accesstoken])
     return (
         <div className='login'>
             <div className="login__container">
                 <img src="http://pngimg.com/uploads/youtube/youtube_PNG102354.png" alt=""/>
-                <button>Login with Google</button>
+                <button onClick={handleLogin}>Login with Google</button>
                 <p>This is a youtube clone project</p>
             </div>
         </div>
