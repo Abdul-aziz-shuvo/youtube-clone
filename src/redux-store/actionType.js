665283db77c723ffe4bc4a19b5031c11fa90695b
Abdul-aziz-shuvo <shuvoahmedkhan202@gmail.com>
2021-05-31 18:57:30 +0600

    all action type file

---
+++ src/redux-store/actionType.js
@@ -0,0 +1,10 @@
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS"
+export const LOGIN_FAIL = "LOGIN_FAIL"
+export const LOGIN_REQUEST = "LOGIN_REQUEST"
+export const LOG_OUT = "LOG_OUT"
+export const LOAD_PROFILE = "LOAD_PROFILE"
+
+
+export const HOME_VIDEO_REQUEST = 'HOME_VIDEO_REQUEST'
+export const HOME_VIDEO_SUCCESS = 'HOME_VIDEO_SUCCESS'
+export const HOME_VIDEO_FAIL = 'HOME_VIDEO_FAIL'
